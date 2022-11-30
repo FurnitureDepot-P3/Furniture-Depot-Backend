28cb488864e49de240787927f6676cf6edf897bb
Jason Carmichael <jasonc0308@icloud.com>
2022-11-30 11:17:59 -0600

    db migrate models

--- models/category.js
+++ models/category.js
@@ -14,7 +14,10 @@ module.exports = (sequelize, DataTypes) => {
     }
   }
   Category.init({
-    name: DataTypes.STRING
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   }, {
     sequelize,
     modelName: 'Category',
