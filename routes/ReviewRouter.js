fa266ed7af8e535823acdc8f22d2fef67c703220
Jason Carmichael <jasonc0308@icloud.com>
2022-12-01 17:30:16 -0600

    update review working per insomnia

--- routes/ReviewRouter.js
+++ routes/ReviewRouter.js
@@ -3,6 +3,7 @@ const controller = require('../controllers/ReviewController')
 
 Router.get('/', controller.GetReviews)
 // Router.post('/:product_id', controller.CreateReview)
+Router.put('/:id', controller.UpdateReview)
 Router.delete('/:id', controller.DeleteReview)
 
 module.exports = Router
