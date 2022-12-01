178b64a04b62829c7bd4b14bab34111460f52b53
Jason Carmichael <jasonc0308@icloud.com>
2022-12-01 17:15:52 -0600

    delete review working per insomnia

--- controllers/ReviewController.js
+++ controllers/ReviewController.js
@@ -9,19 +9,17 @@ const GetReviews = async (req, res) => {
     }
 }
 
-// const DeleteReview = async (req, res) => {
-//     try {
-//       let reviewId = parseInt(req.params.id)
-//       await Review.destroy({where: { id: reviewId}})
-//       res.send({ message: `Deleted review with an id of ${reviewId}`})
-//     } catch (error) {
-//       throw error
-//     }
-//   }
+const DeleteReview = async (req, res) => {
+    try {
+      let reviewId = parseInt(req.params.id)
+      await Review.destroy({where: { id: reviewId}})
+      res.send({ message: `Deleted review with an id of ${reviewId}`})
+    } catch (error) {
+      throw error
+    }
+  }
 
 module.exports = {
     GetReviews,
-    // CreateReview,
-    // DeleteReview
-
+    DeleteReview
 }
