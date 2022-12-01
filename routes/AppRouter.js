8a303d86a0fbf9eed6c57ed7cea48fb75de2f976
Jason Carmichael <jasonc0308@icloud.com>
2022-12-01 11:39:07 -0600

    deleted routes from app,js file

--- routes/AppRouter.js
+++ routes/AppRouter.js
@@ -3,8 +3,10 @@ const CategoryRouter = require('./CategoryRouter')
 const ProductRouter = require('./ProductRouter')
 const ReviewRouter = require('./ReviewRouter')
 const UserRouter = require('./UserRouter')
+
 Router.use('/categories', CategoryRouter)
 Router.use('/products', ProductRouter)
 Router.use('/reviews', ReviewRouter)
 Router.use('/users', UserRouter)
+
 module.exports = Router
