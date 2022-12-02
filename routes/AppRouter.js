8a25888c3168d3db794e572a2cae75bf5d361bf3
Matt <mkiska1@gmail.com>
2022-12-02 11:08:05 -0500

    added Auth, installed bcrypt+jsonwebtoken

--- routes/AppRouter.js
+++ routes/AppRouter.js
@@ -3,10 +3,12 @@ const CategoryRouter = require('./CategoryRouter')
 const ProductRouter = require('./ProductRouter')
 const ReviewRouter = require('./ReviewRouter')
 const UserRouter = require('./UserRouter')
+const AuthRouter = require('./AuthRouter')
 
 Router.use('/categories', CategoryRouter)
 Router.use('/products', ProductRouter)
 Router.use('/reviews', ReviewRouter)
 Router.use('/users', UserRouter)
+Router.use('/auth', AuthRouter)
 
 module.exports = Router
