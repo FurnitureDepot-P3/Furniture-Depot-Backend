51dd52583746269e882647991d69b283d9af8713
Jason Carmichael <jasonc0308@icloud.com>
2022-12-02 15:25:38 -0600

    seed file for three users

---
+++ seeders/20221202205749-user.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const { query } = require('express');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('users', 
+    [
+      {
+        username: 'Darth Vader',
+        email: 'dv1@deathstar.com',
+        password: 'Iamtheboss',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        username: 'Ted Lasso',
+        email: 'coach@.afcrichmond.com',
+        password: 'callmecoach',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        username: 'James Bond',
+        email: 'jb007@.misix.com',
+        password: 'topsecret',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ], 
+    {}
+    )
+  },
+
+  async down (queryInterface, Sequelize) {
+   return queryInterface.bulkDelete('users', null, {})
+  }
+};
