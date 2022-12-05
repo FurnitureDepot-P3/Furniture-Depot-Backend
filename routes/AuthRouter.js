8c43b6fd5e552bcb58ba6c877f669c5cae9274dc
Matt <mkiska1@gmail.com>
2022-12-05 16:34:58 -0500

    updated to make signin/register work

--- routes/AuthRouter.js
+++ routes/AuthRouter.js
@@ -2,7 +2,7 @@ const Router = require('express').Router()
 const controller = require('../controllers/AuthController')
 const middleware = require('../middleware')
 
-Router.post('/login', controller.Login)
+Router.post('/signin', controller.Login)
 Router.post('/register', controller.Register)
 Router.post(
   '/update',
