ec4491ea0c20d3a3bf9c690cdc25d7aa72cea1a2
Matt <mkiska1@gmail.com>
2022-12-06 14:27:48 -0500

    updated BLOB in products to TEXT for images.

---
+++ migrations/20221206192450-change-blob-to-text-products.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.changeColumn('products', 'image', {
+      type: Sequelize.TEXT
+    })
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.changeColumn('products', 'image', {
+      type: Sequelize.BLOB
+    })
+  }
+}
