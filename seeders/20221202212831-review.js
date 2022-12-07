fdcb09d3814c0267df641a5de9d1bb0d802bbc9f
Matt <mkiska1@gmail.com>
2022-12-07 12:05:24 -0500

    added more seeds for products and reviews.

--- seeders/20221202212831-review.js
+++ seeders/20221202212831-review.js
@@ -32,8 +32,8 @@ module.exports = {
       {
         user_id: 3,
         product_id: 4,
-        rating: 3,
-        comment: 'difficult to assemble. I just wanted to relax!',
+        rating: 4,
+        comment: 'Great for relaxing on a warm summer day with a few beers',
         createdAt: new Date(),
         updatedAt: new Date()
       },
@@ -42,6 +42,70 @@ module.exports = {
         product_id: 4,
         rating: 5,
         comment: 'arrived in a jiffy.',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        user_id: 3,
+        product_id: 5,
+        rating: 5,
+        comment: 'This bed is very comfy. I like the color too.',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        user_id: 1,
+        product_id: 6,
+        rating: 3, 
+        comment: 'I like the table, but the legs are a little wobbly',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        user_id: 2,
+        product_id: 7,
+        rating: 2,
+        comment: 'Fire pit is nice but my dog got a little too close and singed some tail hair.',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        user_id: 4,
+        product_id: 8,
+        rating: 5,
+        comment: 'This thing really lights up the room, love the vintage feel as well.',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        user_id: 1,
+        product_id: 9,
+        rating: 4,
+        comment: 'Solid nightstand, nothing to complain about.',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        user_id: 2,
+        product_id: 10,
+        rating: 5,
+        comment: 'I love this thing, it really brings the room together.',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        user_id: 1,
+        product_id: 11,
+        rating: 4,
+        comment: 'Nice dresser, matches my nightstand well.',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        user_id: 3,
+        product_id: 12,
+        rating: '3',
+        comment: 'difficult to assemble, but once it was done it was a nice addition to the backyard',
         createdAt: new Date(),
         updatedAt: new Date()
       }
